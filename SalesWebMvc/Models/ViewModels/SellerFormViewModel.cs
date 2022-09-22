29ccb21761b7faf66f9d277b1aa20990d4eeb054
Douglas da Silva Nascimento <dnascimento@enavirj.com.br>
2022-09-22 16:14:42 -0300

    SellerFormViewModel and Department select component

---
+++ SalesWebMvc/Models/ViewModels/SellerFormViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace SalesWebMvc.Models.ViewModels
+{
+    // Classe que servirá para criarmos o formulários de cadastro de vendedor
+    public class SellerFormViewModel
+    {
+        public Seller Seller { get; set; }
+        public ICollection<Department> Departments { get; set; }
+    }
+}
---
+++ SalesWebMvc/Services/DepartmentService.cs
@@ -0,0 +1,24 @@
+﻿using SalesWebMvc.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMvc.Services
+{
+    public class DepartmentService
+    {
+        // Injeção de dependencia
+        private SalesWebMvcContext _context;
+
+        public DepartmentService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public List<Department> FindAll()
+        {   // Pegando a lista de departamento ordenada por nome.
+            return _context.Department.OrderBy(x => x.Name).ToList();
+        }
+
+
+    }
+}
