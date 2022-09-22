4c653f98dd2f08b6d1620fbbe190dfb3ca11175c
Douglas da Silva Nascimento <dnascimento@enavirj.com.br>
2022-09-22 15:29:57 -0300

    Foreign key not null

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -21,6 +21,7 @@ namespace SalesWebMvc.Services
         // Classe responsavel por criar/inserir os objetos no banco de dados.
         public void Insert(Seller obj)
         {
+            obj.Department = _context.Department.First();
             _context.Add(obj);
             _context.SaveChanges();
         }
